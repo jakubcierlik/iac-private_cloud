12dae099ac3228ffed5d7006202a31ad414b20a3
Jakub Cierlik <jakub.cierlik@amartus.com>
2022-06-28 14:52:38 +0200

    separated variables tf files for trustgrid-dev and trustgrid-prod

---
+++ trustgrid-prod/variables.tf
@@ -0,0 +1,41 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "certificate-private_key" {
+  type = string
+  sensitive = true
+}
+variable "certificate-ssc" {
+  type = string
+  sensitive = true
+}
+variable "certificate-ca_bundle" {
+  type = string
+  sensitive = true
+}
+
+variable "aws_vpc_ssh_public_key_prod" {
+  type = string
+  sensitive = true
+}
+variable "aws_vpc_ssh_public_key_dev" {
+  type = string
+  sensitive = true
+}
+variable "node_license-prod-01" {
+  type = string
+  sensitive = true
+}
+variable "node_license-prod-02" {
+  type = string
+  sensitive = true
+}
+variable "node_license-dev-01" {
+  type = string
+  sensitive = true
+}
+variable "node_license-dev-02" {
+  type = string
+  sensitive = true
+}
